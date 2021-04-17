1962fb5d91dcda9a77384f298cedfb4c63199855
Kamil Krzempek <kamilkrzempek99@gmail.com>
2021-04-17 17:40:51 +0200

    Add mock routes

--- drinol/frontend/src/views/Root/Root.tsx
+++ drinol/frontend/src/views/Root/Root.tsx
@@ -1,72 +1,29 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import axios from "axios";
 
-import { ExampleComponent } from "components";
 import { APP_ROUTES } from "commons/appRoutes";
-import createTokenQuery from "commons/auth/tokenQuery";
-import { TokenContext } from "commons/auth/tokenContext";
+// import { Room } from "views";
 
 import "./Root.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      // suspense: true,
-    },
-  },
-});
-
-// const tokenQuery = createTokenQuery<
-//   { access: string; refresh: string },
-//   { username: string; password: string }
-// >({
-//   queryKey: "token",
-//   tokenExpired: ({ access }) =>
-//     axios
-//       .post("/api/auth/jwt/verify/", {
-//         token: access,
-//       })
-//       .then(({ data }) => data.code === "token_not_valid"),
-//   refreshExpired: ({ refresh }) =>
-//     axios
-//       .post("/api/auth/jwt/verify/", {
-//         token: refresh,
-//       })
-//       .then(({ data }) => data.code === "token_not_valid"),
-//   sendLogin: (loginParams) =>
-//     axios.post("/api/auth/jwt/create/", loginParams).then(({ data }) => data),
-//   sendRefresh: ({ refresh }) =>
-//     axios
-//       .post("/api/auth/jwt/refresh/", {
-//         refresh,
-//       })
-//       .then(({ data }) => data),
-//   retry: () => false,
-//   refreshExpiredError: new Error("401-Refresh token expired"),
-//   shouldRefreshOnBackground: () => false,
-// });
+const queryClient = new QueryClient();
 
 export const Root = () => {
-  // useEffect(() => {
-  //   tokenQuery.init(queryClient);
-  // }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
-      {/*<TokenContext.Provider value={tokenQuery}>*/}
       <Suspense fallback={<div>Loading...</div>}>
         <Router>
           <Switch>
             <Route path={APP_ROUTES.MAIN} exact>
-              <ExampleComponent />
+              <div>Main route</div>
+            </Route>
+            <Route path={APP_ROUTES.SETTINGS} exact>
+              <div>Settings route</div>
             </Route>
           </Switch>
         </Router>
       </Suspense>
-      {/*</TokenContext.Provider>*/}
     </QueryClientProvider>
   );
 };
